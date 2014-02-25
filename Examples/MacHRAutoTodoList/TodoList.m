e68cbaa82fa2ebc8a73a3f33d667092541954d7d
Alex Gray <alex@mrgray.com>
2014-02-24 20:23:15 -0500

    Cleaned up Mac Demo, reducing SLOC, clarifying, etc.
    Also fixes an erroneous Xcode "validation" warning.  All functionality
    retained + tested at 100%.

--- Examples/MacHRAutoTodoList/TodoList.m
+++ Examples/MacHRAutoTodoList/TodoList.m
@@ -1,37 +1,33 @@
 
-//  TodoList.m
-//  TodoListExample
-
-//  Created by Alex Gray on 11/26/12.
-//	Part of BaseModel by Nick Lockwood.
-
+//  TodoList.m -  TodoListExample
+//  Created by Alex Gray on 11/26/12. -	Part of BaseModel by Nick Lockwood.
 
 #import "Todos.h"
 
 @implementation TodoList
 
-/*	note: we've not implemented the NSCoding methods or setWithArray/Dictionary, etc
-	because the HRCoder and AutoCoding libraries take care of this for us 	*/
-
+/*!	@note we've not implemented the NSCoding methods or setWithArray/Dictionary, etc
+          because the HRCoder and AutoCoding libraries take care of this for us 	
+*/
 - (TodoItem*) newTodo
 {
 	TodoItem *newOne = TodoItem.instance;
-	[self insertObject:newOne inItemsAtIndex:self.items.count];  // KVO Array insertion trigger.
+	[self insertObject:newOne inItemsAtIndex:_items.count];  // KVO Array insertion trigger.
 	return newOne;
 }
-- (TodoItem*) copyTodo: (TodoItem*)todo;
+
+- (TodoItem*) copyTodo:(TodoItem*)todo
 {
 	TodoItem *newOne = todo.copy;
-	[self insertObject:newOne inItemsAtIndex:self.items.count];
+	[self insertObject:newOne inItemsAtIndex:_items.count];
 	return newOne;
 }
 
-- (NSUInteger)countOfItems { 					return self.items.count; }
+/*! Subclass specific KVO Compliant "items" accessors to trigger NSArrayController updates on inserts / removals. */
 
-- (id)objectInItemsAtIndex:(NSUInteger)index {	return self.items[index];	 }
-
-- (void)removeObjectFromItemsAtIndex:(NSUInteger)index {				 	[self.items removeObjectAtIndex:index];	}
-
-- (void)insertObject:(TodoItem*)todo inItemsAtIndex:(NSUInteger)index {	[self.items insertObject:todo atIndex:index];	}
+- (NSUInteger)                             countOfItems               { return self.items.count; }
+-         (id)                     objectInItemsAtIndex:(NSUInteger)i {	return self.items[i];	}
+-       (void)             removeObjectFromItemsAtIndex:(NSUInteger)i {	[self.items removeObjectAtIndex:i];	}
+-       (void) insertObject:(TodoItem*)t inItemsAtIndex:(NSUInteger)i {	[self.items insertObject:t atIndex:i];	}
 
 @end
